414d47e92e12672a6d30dda3b9c1985048b5257e
lilly liu <lillyliu@berkeley.edu>
2024-01-23 12:39:07 -0800

    beginning edits

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -18,7 +18,7 @@ export const help = async (args: string[]): Promise<string> => {
 \n${c}\n
 [tab]: trigger completion.
 [ctrl+l]/clear: clear terminal.\n
-Type 'sumfetch' to display summary.
+Type 'sumfetch' to display a summary.
 `;
 };
 
@@ -141,17 +141,20 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+  █████        ███  ████  ████                █████        ███            
+  ░░███        ░░░  ░░███ ░░███               ░░███        ░░░             
+   ░███        ████  ░███  ░███  █████ ████    ░███        ████  █████ ████
+   ░███       ░░███  ░███  ░███ ░░███ ░███     ░███       ░░███ ░░███ ░███ 
+   ░███        ░███  ░███  ░███  ░███ ░███     ░███        ░███  ░███ ░███ 
+   ░███      █ ░███  ░███  ░███  ░███ ░███     ░███      █ ░███  ░███ ░███ 
+   ███████████ █████ █████ █████ ░░███████     ███████████ █████ ░░████████
+  ░░░░░░░░░░░ ░░░░░ ░░░░░ ░░░░░   ░░░░░███    ░░░░░░░░░░░ ░░░░░   ░░░░░░░░ 
+                                  ███ ░███                                 
+                                 ░░██████                                  
+                                  ░░░░░░                                                          
 
+Type 'sumfetch' to display a short summary of my profile.
 Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for my Github.
 `;
 };
